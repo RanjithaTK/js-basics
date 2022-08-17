1cc426cced0c64f40e92ac0235a68f67434e4902
im ranjitha <ranjithatk101@gmail.com>
2022-08-18 05:24:25 +0530

    datatypes

---
+++ dataTypes.js
@@ -0,0 +1,6 @@
+//string,integer,boolean,undefined,null,object,bigInt.
+//integer+string=>string
+//"Ranji"+12=>Ranji12
+let car;    // Value is undefined, type is undefined
+car = undefined;    // Value is undefined, type is undefined
+let car1 = "";    // The value is "", the typeof is "string"
